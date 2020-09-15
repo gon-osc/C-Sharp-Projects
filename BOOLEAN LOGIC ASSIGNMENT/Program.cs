7190db90eebccfb9167d47321ed317a74bebf30a
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-15 14:00:44 -0600

    Add files via upload

---
+++ BOOLEAN LOGIC ASSIGNMENT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOLEAN_LOGIC_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Qualification Questions");
+
+            Console.WriteLine("What is your age?");
+            int clientAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please answer 'true' or 'false'");
+            string userDui = Console.ReadLine();
+            bool userInput = Convert.ToBoolean(userDui);
+
+            Console.WriteLine("How many speeding tickets do you have??");
+            int clientTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool insApproval = clientAge >= 15 && userInput == false && clientTickets <= 3;
+
+            Console.WriteLine("Were you qualified today?");
+            Console.WriteLine(insApproval);
+            Console.ReadLine();
+        }
+    }
+}
