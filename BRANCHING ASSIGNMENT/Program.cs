488cdd3f8b6c2fd02d66c295a3f09f072a1a1626
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-16 16:23:30 -0600

    Add files via upload

--- BRANCHING ASSIGNMENT/Program.cs
+++ BRANCHING ASSIGNMENT/Program.cs
@@ -14,27 +14,32 @@ namespace BRANCHING_ASSIGNMENT
             Console.WriteLine("Welcome to Package Express.Please follow the instructions below.");
 
             Console.WriteLine("Please enter the package weight:");
-            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            int weight = Convert.ToInt32(Console.ReadLine());
+     
+            if (weight >= 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("please enter the package width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
 
-            string results = packageWeight > 50 ? "Package too heavy to be shipped via Package Express.Have a good day." : "please enter the package width:" ;
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(results);
-   
-            int packageWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
 
-            Console.WriteLine("Please enter the package height:");
-            int packageHeight = Convert.ToInt32(Console.ReadLine());
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package length:");
+                int packageDimension = width + height + length;
+                int packagePricing = width * height * length * weight / 100;
 
-            int packageLength = Convert.ToInt32(Console.ReadLine());
+                string dimensions = packageDimension > 50 ? "Package too big to be shipped via Package Express." : "Your estimated total for shipping this package is: $" + String.Format("{0:0.00}", packagePricing);
 
-            int packageDimension = packageWidth * packageHeight * packageLength * packageWeight;
-            int shippingTotal = packageDimension / 100;
-
-            string dimensions = packageWidth > 50 || packageHeight > 50 || packageLength > 50 ? "Package too big to be shipped via Package Express." : "Your estimated total for shipping this package is: $" + String.Format("{0:0.00}", shippingTotal);
-
-            Console.WriteLine(dimensions);
+                Console.WriteLine(dimensions);
+            }
+            
             Console.WriteLine("Thank you!");
             Console.ReadLine();
 
