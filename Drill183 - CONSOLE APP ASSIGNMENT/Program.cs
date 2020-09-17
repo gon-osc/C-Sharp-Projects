31cfb3602ae7c6840a7d585919c597ce0768a161
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-17 14:28:36 -0600

    Add files via upload

--- Drill183 - CONSOLE APP ASSIGNMENT/Program.cs
+++ Drill183 - CONSOLE APP ASSIGNMENT/Program.cs
@@ -10,42 +10,49 @@ namespace Drill183___CONSOLE_APP_ASSIGNMENT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is nine minus 3 divided by one-third plus one?");
-            int number = Convert.ToInt32(Console.ReadLine());
-            bool answerIs = number == 1;
+            //Console.WriteLine("What is nine minus 3 divided by one-third plus one?");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool answerIs = number == 1;
 
-            do
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        case 3:
+            //            Console.WriteLine("You guessed 3. Try Again.");
+            //            Console.WriteLine("Hint: PEMDAS.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("You guessed 7. Try Again.");
+            //            Console.WriteLine("Hint: PEMDAS.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 9:
+            //            Console.WriteLine("You guessed 9. Try Again.");
+            //            Console.WriteLine("Hint: PEMDAS.");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 1:
+            //            Console.WriteLine("That is correct!. After applying PEMDAS the correct answer is 1.");
+            //            answerIs = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are wrong.");
+            //            Console.WriteLine("Try Again, Dont give up!");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //    }
+            //}
+            //while (!answerIs);
+
+            int myNumber = 10;
+            while(myNumber <= 20)
             {
-                switch (number)
-                {
-                    case 3:
-                        Console.WriteLine("You guessed 3. Try Again.");
-                        Console.WriteLine("Hint: PEMDAS.");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 7:
-                        Console.WriteLine("You guessed 7. Try Again.");
-                        Console.WriteLine("Hint: PEMDAS.");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 9:
-                        Console.WriteLine("You guessed 9. Try Again.");
-                        Console.WriteLine("Hint: PEMDAS.");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 1:
-                        Console.WriteLine("That is correct!. After applying PEMDAS the correct answer is 1.");
-                        answerIs = true;
-                        break;
-                    default:
-                        Console.WriteLine("You are wrong.");
-                        Console.WriteLine("Try Again, Dont give up!");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-
-                }
+                Console.WriteLine(myNumber);
+                myNumber++;   
             }
-            while (!answerIs);
 
             Console.Read();
 
