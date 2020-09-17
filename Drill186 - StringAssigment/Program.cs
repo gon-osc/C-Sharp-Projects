147c7122a77c4c2636abd23eeda16c730eacc3ee
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-17 15:18:00 -0600

    Add files via upload

---
+++ Drill186 - StringAssigment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill186___StringAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is you last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What state are you from?");
+            string homeState = Console.ReadLine();
+            homeState = homeState.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Welcome " + firstName);
+            sb.Append(" "+lastName);
+            sb.Append(" from ");
+            sb.Append(homeState + ".");
+            sb.AppendLine();
+            sb.Append("How can we help you today?");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
