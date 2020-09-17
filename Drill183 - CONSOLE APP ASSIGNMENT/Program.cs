074d1d5b9831376b433ea737d4ebdfb43810b995
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-16 21:22:21 -0600

    Add files via upload

---
+++ Drill183 - CONSOLE APP ASSIGNMENT/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill183___CONSOLE_APP_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is nine minus 3 divided by one-third plus one?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool answerIs = number == 1;
+
+            do
+            {
+                switch (number)
+                {
+                    case 3:
+                        Console.WriteLine("You guessed 3. Try Again.");
+                        Console.WriteLine("Hint: PEMDAS.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("You guessed 7. Try Again.");
+                        Console.WriteLine("Hint: PEMDAS.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("You guessed 9. Try Again.");
+                        Console.WriteLine("Hint: PEMDAS.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.WriteLine("That is correct!. After applying PEMDAS the correct answer is 1.");
+                        answerIs = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Try Again, Dont give up!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!answerIs);
+
+            Console.Read();
+
+        }
+    }
+}
