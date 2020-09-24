ecfd58546d018e2faee73cdb29f20acbebaf1200
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-23 18:29:58 -0600

    Add files via upload

---
+++ Drill216 - ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill216___ASSIGNMENT
+{
+    class MathOperations
+    {
+        public int sum(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int result = a + b;
+
+            return result;
+        }
+        public int mult(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int result = a * b;
+
+            return result;
+        }
+        public int div(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int result = b / a;
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public int Sum(int x, int y)
+        //{
+        //        int a = x;
+        //        int b = y;
+        //        int result = a + b;
+
+        //        return result;           
+
+        //}
+        //public class Multiplication()
+        //{
+
+        //}
+        //public class Subtraction()
+        //{
+
+        //}
+    }
+}
---
+++ Drill216 - ASSIGNMENT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill216___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a number you want to do the math operations on.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            int a = userNumber;
+            int b = 100;
+            int Stotal;
+            int Mtotal;
+            int Dtotal;
+
+            MathOperations n = new MathOperations();       
+            Stotal = n.sum(a, b);
+            Mtotal = n.mult(a, b);
+            Dtotal = n.div(a, b);
+
+            Console.WriteLine("100 plus " + a + " equals : {0}", Stotal);
+            
+            Console.WriteLine("100 multiplied by " + a + " equal: {0}", Mtotal);
+            
+            Console.WriteLine("100 divided by " + a + " equal: {0}", Dtotal);
+            
+            Console.ReadLine();
+        }
+    }
+}
