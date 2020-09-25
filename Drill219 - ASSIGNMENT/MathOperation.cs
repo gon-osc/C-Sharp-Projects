12ff3c6982bc608d24a81e10650b42867b497786
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-25 10:04:52 -0600

    Add files via upload

---
+++ Drill219 - ASSIGNMENT/MathOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill219___ASSIGNMENT
+{
+    class MathOperation
+    {
+        public int mult(int x, int y)
+        {
+            int a = x;
+            int b = y;
+            int result = a * b;
+
+            return result;
+        }
+        public double Sub(double m, double n)
+        {
+            double c = m;
+            double d = n;
+            double result = c - d;
+
+            return result;
+        }
+        public int Add(int w, int z)
+        {
+            int num1 = w;
+            int num2 = z;
+            int add = num1 + num2;
+
+            return add;
+        }
+    }
+}
---
+++ Drill219 - ASSIGNMENT/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill219___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            int a = 100;
+            int b = 10;
+            int Mtotal;
+
+            double d = 42.35;
+            double c = 99.99;
+            double Dtotal;
+
+            string f = "45";
+            string g = "35";
+            int num1 = Int32.Parse(f);
+            int num2 = Int32.Parse(g);
+            int Atotal;
+
+
+            MathOperation n = new MathOperation();
+            Mtotal = n.mult(a, b);
+            Dtotal = n.Sub(c, d);
+            Atotal = n.Add(num1, num2);
+
+
+            Console.WriteLine("100 multiplied by 10 equal: {0}", Mtotal);
+            Console.WriteLine("99.99 minus 45.99 equal: {0}", Dtotal);
+            Console.WriteLine(f + " plus " +g+ " equal: {0}", Atotal);
+
+
+            Console.ReadLine();
+        }
+    }
+}
