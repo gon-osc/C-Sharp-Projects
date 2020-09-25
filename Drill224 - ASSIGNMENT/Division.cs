6362912708b0f6b44158ae9e3ea9ad1e8bc2cedb
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-25 12:58:43 -0600

    Add files via upload

---
+++ Drill224 - ASSIGNMENT/Division.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill224___ASSIGNMENT
+{
+    class Division
+    {
+        public void Div(int userNumber)
+        {
+            Console.WriteLine("Sum result: {0}", userNumber / 2);
+        }
+        public void Div(decimal userNumber)
+        {
+            Console.WriteLine("Sum result: {0}", userNumber / 2);
+        }
+        public void getNumber(out int a)
+        {
+            int temp = 9;
+            a = temp;
+        }
+    }
+    static class Example
+    {
+        public static string phrase = "Static classes are sealed, means one cannot inherit a static class from another class.";
+    }
+}
---
+++ Drill224 - ASSIGNMENT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill224___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Division n = new Division();
+
+            Console.WriteLine("Division");
+            Console.WriteLine("Pick a number to do divide in half.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            n.Div(userNumber);
+
+            Console.WriteLine("Pick a number to do divide in half.");
+            decimal userNumber2 = decimal.Parse(Console.ReadLine());           
+            n.Div(userNumber2);
+
+            int a = 25;
+            Console.WriteLine("Before Output Method called value of a was: {0}", a);
+            n.getNumber(out a);
+            Console.WriteLine("After Output Method called value of a is now: {0}", a);
+
+            Console.WriteLine("This was called from within a static class: {0} ", Example.phrase);
+
+            Console.ReadLine();
+        }
+    }
+}
