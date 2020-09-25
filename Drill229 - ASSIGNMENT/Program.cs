0c20f5383be8e81b5b4ca524138d371fa1c5196e
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-25 14:12:20 -0600

    Add files via upload

---
+++ Drill229 - ASSIGNMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill229___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.Id = 101;
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
