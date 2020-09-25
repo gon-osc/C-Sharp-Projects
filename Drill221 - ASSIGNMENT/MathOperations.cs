c2e046a43ca7170ccabdb80c0de683e5795eab05
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-25 10:06:03 -0600

    Add files via upload

---
+++ Drill221 - ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill221___ASSIGNMENT
+{
+    class MathOperations
+    {
+        public void Sum( int first, int second)
+        {
+            Console.WriteLine("Sum result: {0}", first + second);
+                                  
+            Console.WriteLine("The number you chose was: " + second);
+        }
+
+    }
+}
---
+++ Drill221 - ASSIGNMENT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill221___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations n = new MathOperations();
+
+            Console.WriteLine("Addition");
+            Console.WriteLine("Pick two number to add");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+           
+
+            n.Sum(firstNumber, secondNumber);
+
+            n.Sum(first: 5, second: 9);
+
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
