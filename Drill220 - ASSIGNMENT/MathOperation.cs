d74a7150c20217fd12885c15731db4b45f144ca1
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-25 10:05:26 -0600

    Add files via upload

---
+++ Drill220 - ASSIGNMENT/MathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill220___ASSIGNMENT
+{
+    class MathOperation
+    {
+        public int multNumber(int firstNumber, int secondNumber = 6)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
---
+++ Drill220 - ASSIGNMENT/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill220___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            MathOperation n = new MathOperation();
+
+            Console.WriteLine("Multiplication");
+            Console.WriteLine("Pick the first number");
+            int firstNumber =  Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Pick the second number to multiply by... Unless otherwise leave empty and our default number will be selected");
+            var sNumberIsValid = int.TryParse(Console.ReadLine(), out int sNumber);
+
+            int total;
+            if (sNumberIsValid)
+            {
+                total = n.multNumber(firstNumber, sNumber);
+                Console.WriteLine(firstNumber + " * " + sNumber + " is: {0} ", total);
+            }
+            else
+            {
+                total = n.multNumber(firstNumber);
+                Console.WriteLine(firstNumber + " * 6 total is: {0} ", total);
+            }
+                        
+            ;
+
+
+            Console.ReadLine();
+        }
+    }
+}
