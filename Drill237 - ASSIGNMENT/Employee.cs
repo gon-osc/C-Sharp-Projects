26f514563e5b1f3119e654d4942b8dee1beabd30
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-09-28 10:21:23 -0600

    Add files via upload

---
+++ Drill237 - ASSIGNMENT/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill237___ASSIGNMENT
+{
+    class Employee:Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Welcome");
+            base.SayName();
+        }
+    }
+}
