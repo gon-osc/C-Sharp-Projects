7d54df746a12a2d0a74a038ed46c208081e44330
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-10-01 10:58:26 -0600

    Add files via upload

---
+++ Drill293 - ASSIGNMENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Drill293___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine(date1);
+            
+            Console.WriteLine("Please input a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            DateTime futTime = date1.AddHours(userNumber);
+            Console.WriteLine("Based on you number it will be {0} in {1} hours.", futTime, userNumber);
+            
+            Console.ReadLine();
+        }
+    }
+}
