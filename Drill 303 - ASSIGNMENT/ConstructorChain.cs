b6b391a28500192ac8ee684dad65abbadfded0f1
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-10-05 08:44:04 -0600

    Add files via upload

---
+++ Drill 303 - ASSIGNMENT/ConstructorChain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_303___ASSIGNMENT
+{
+    class ConstructorChain
+    {
+        public int studentAge;
+        public string studentName;
+
+        public ConstructorChain(int studentAge)
+        {
+            this.studentAge = studentAge;
+            Console.WriteLine("" + studentAge);
+        }
+        public ConstructorChain(int studentAge, string studentName): this(studentAge)
+        {
+            this.studentName = studentName;
+            Console.WriteLine("" + studentName);
+        }
+    }
+}
---
+++ Drill 303 - ASSIGNMENT/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_303___ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string programName = "C#";
+
+            var releaseYear = 2000;
+
+            Console.WriteLine("{0} has evolved much since their first appearance in the year {1}. The current version of C# is 9.0.", programName, releaseYear);
+
+
+            ConstructorChain obj = new ConstructorChain(26, "James");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
