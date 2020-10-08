3590be38804233e9b0e6edb4816a87d3a2e7b6e7
Oscar Gonzalez <60861465+gon-osc@users.noreply.github.com>
2020-10-08 17:43:00 -0600

    Add files via upload

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using(InsuranceEntities db = new InsuranceEntities())
+            {
+                var issuedPol = db.Insurees;
+                var newPol = new List<Quotesvm>();
+                foreach (var policies in issuedPol)
+                {
+                    var issNewPol = new Quotesvm();
+                    issNewPol.FirstName = policies.FirstName;
+                    issNewPol.LastName = policies.LastName;
+                    issNewPol.EmailAddress = policies.EmailAddress;
+                    issNewPol.Quote = policies.Quote;
+                    newPol.Add(issNewPol);
+                }
+                return View(newPol);
+            }
+        }
+    }
+}
